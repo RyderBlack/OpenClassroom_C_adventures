7ad97307e512d69c600ca8464cc5757752cf65fe
rydeblack <ryhad.boughanmi@gmail.com>
2025-02-25 16:08:40 +0100

    init: initialize repo

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+
+    // int nombreDeVies = 9; // Au départ, le joueur a 5 vies
+    // int age = 0;
+    // printf("Vous avez %d vies\n", nombreDeVies);
+    // printf("**** B A M ****\n"); // Là il se prend un grand coup sur la tête
+    // nombreDeVies = 8; // Il vient de perdre une vie !
+    // scanf("%d", &age);
+    // printf("Ah desole, il ne vous reste plus que %d vies maintenant !\n\n", nombreDeVies);
+
+
+    // addition
+    // int result = 0, number1 = 0, number2 = 0;
+    // printf("enter a number: ");
+    // scanf("%d", &number1);
+    //
+    // printf("enter another number: ");
+    // scanf("%d", &number2);
+    //
+    // result = number1 + number2;
+    //
+    // printf("%d + %d = %d\n", number1, number2,result);
+
+
+    //If Else Switch
+
+    int menuNumber = 0;
+    printf("=== Menu ===");
+    printf("Enter a number to choose a meal.\n 1. Royal Cheese \n 2. Mc Deluxe \n 3. Mc Bacon \n 4. Big Mac \n Votre choix ? \n");
+    scanf("%d", &menuNumber);
+    printf("")
+
+    switch (menuNumber) {
+        case 1:
+            printf("Vous avez choisi le Royal Cheese");
+        break;
+        case 2:
+            printf("Vous avez choisi le Mc Deluxe");
+        break;
+        case 3:
+            printf("Vous avez choisi le Mc Bacon");
+        break;
+        case 4:
+            printf("Vous avez choisi le Big Mac");
+        break;
+        default:
+            printf("Enter a valid menu number");
+        break;
+    }
+
+
+
+
+    return 0;
+}
+
