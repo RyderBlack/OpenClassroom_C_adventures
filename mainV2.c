5c3b02a5279b2a30b940394f850d9746fc687f15
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-01 12:09:59 +0100

    feat: repeat func added

--- mainV2.c
+++ mainV2.c
@@ -10,17 +10,28 @@ int doubling(int num)
   return result;
 }
 
+void repeatAfterMe(int lineNumber)
+{
+
+    for (int i = 0 ; i < lineNumber ; i++)
+    {
+        printf("I am the one and only C master\n");
+    }
+}
+
+
+
 int main(int argc, char *argv[]) {
 
-    int enteredNumber = 0, doubledNumber = 0;
+    // int enteredNumber = 0, doubledNumber = 0;
+    //
+    // printf("Enter a number: ");
+    // scanf("%d", &enteredNumber);
+    //
+    // doubledNumber = doubling(enteredNumber);
+    // printf("Doubled number is: %d\n", doubledNumber);
 
-    printf("Enter a number: ");
-    scanf("%d", &enteredNumber);
-
-    doubledNumber = doubling(enteredNumber);
-    printf("Doubled number is: %d\n", doubledNumber);
-
-
+    repeatAfterMe(10);
     return 0;
 }
 
