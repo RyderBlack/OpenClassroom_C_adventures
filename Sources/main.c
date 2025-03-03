2cb678df5e2aabf4434f10e985e4afd3d88d1516
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-03 15:22:24 +0100

    feat: added triplePointer function to understand pointers

--- Sources/main.c
+++ Sources/main.c
@@ -6,23 +6,30 @@
 
 
 // Prototype:
-
+void triplePointer(int *pointerOnNumber);
 
 int main(int argc, char *argv[])
 {
     // int pointerMysteryNumber = NULL;
-    const int MAX = 100, MIN = 1;
-    srand( time(NULL) );
-    int mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
-    int *pointerMysteryNumber = &mysteryNumber;
+    // const int MAX = 100, MIN = 1;
+    // srand( time(NULL) );
+    // int mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
+    // int *pointerMysteryNumber = &mysteryNumber;
+    //
+    // printf("The number is: %d\n", mysteryNumber);
+    // printf("The number is: %d\n", *pointerMysteryNumber);
 
-    printf("The number is: %d\n", mysteryNumber);
-    printf("The number is: %d\n", *pointerMysteryNumber);
+
+    int number = 5;
+    triplePointer(&number);
+    printf("number is: %d\n", number);
 
     return 0;
 }
 
-
+void triplePointer(int *pointerOnNumber) {
+    *pointerOnNumber *= 3;
+}
 
 
 
