0e3e351c9223eab26eb2ea6fa4048e3bd9fcba2b
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-03 15:16:43 +0100

    feat: added pointer for mysteryNumber variable

--- Sources/main.c
+++ Sources/main.c
@@ -10,12 +10,14 @@
 
 int main(int argc, char *argv[])
 {
+    // int pointerMysteryNumber = NULL;
     const int MAX = 100, MIN = 1;
     srand( time(NULL) );
     int mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
+    int *pointerMysteryNumber = &mysteryNumber;
 
     printf("The number is: %d\n", mysteryNumber);
-
+    printf("The number is: %d\n", *pointerMysteryNumber);
 
     return 0;
 }
