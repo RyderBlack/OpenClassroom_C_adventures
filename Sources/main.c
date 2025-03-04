7391fc8f6b1da3e996a1a8e1b17b95063eafba9b
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-04 15:55:58 +0100

    add array logic into function

--- Sources/main.c
+++ Sources/main.c
@@ -3,24 +3,46 @@
 
 
 // Prototype:
-int showArray(int *array, int size_array);
+void showArray(int array[], int size_array);
+int arrSum(int array[], int size_arr);
+double arrAvg(int array[], int size_arr);
 
 int main(int argc, char *argv[])
 {
 
-    int myArray[4] = { 11,22,33,44}, i = 0;
+    int myArray[4] = { 11,22,33,44};
 
-    showArray(myArray, 4);
-
+    // showArray(myArray, 4);
+    arrSum(myArray, 4);
+    arrAvg(myArray, 4);
     return 0;
 }
 
-int showArray(int array[], int size_array) {
+void showArray(int array[], int size_array) {
     int i = 0;
 
     for (i = 0; i < size_array; i++) {
         printf("array[%d] = %d\n", i, array[i]);
     }
+}
+
+int arrSum(int array[], int size_array) {
+    int sum = 0;
+    for (int i = 0; i < size_array; i++) {
+        sum += array[i];
+    }
+    printf("sum = %d\n", sum);
+    return sum;
+}
+
+double arrAvg(int array[], int size_array) {
+    int sum = 0;
+    for (int i = 0; i < size_array; i++) {
+        sum += array[i];
+    }
+    double formatted_output = (double)sum / (double)size_array;
+    printf("average = %f\n", formatted_output);
+    return formatted_output;
 }
 
 
