9c88be9fa99f98ae3cf5d07e6718f4739eca154d
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-04 15:41:43 +0100

    add array logic

--- Sources/main.c
+++ Sources/main.c
@@ -10,12 +10,12 @@ int main(int argc, char *argv[])
 
     int myArray[4] = { 11,22,33,44}, i = 0;
 
-    showArray()
+    showArray(myArray, 4);
 
     return 0;
 }
 
-int showArray(int *array, int size_array) {
+int showArray(int array[], int size_array) {
     int i = 0;
 
     for (i = 0; i < size_array; i++) {
