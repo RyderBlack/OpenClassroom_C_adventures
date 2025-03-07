8b224ed0db7913a2d3787f0095fbcff2a94452df
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-07 16:02:21 +0100

    structures intro

---
+++ Headers/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+struct Student {
+    char name[100];
+    char surname[100];
+    char city[100];
+    char address[100];
+    int age;
+};
+#endif //MAIN_H
--- Sources/main.c
+++ Sources/main.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE 100
-#define HELLO_WORLD() printf("Hello World\n");
-#define MAJORITY(age) if (age >= 18)\
-                        printf("You are an adult!\n");
-
+#include "main.h"
 // Prototype:
 
 
 int main(int argc, char *argv[])
 {
+    struct Student student;
 
-    struct StudentDatabase {
-        char name[100];
-        int age;
-    };
 
     return 0;
 }
