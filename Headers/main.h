9cb636d223a4ad850d2c75cef7f4b45dc04c95f8
rydeblack <ryhad.boughanmi@gmail.com>
2025-03-07 16:18:08 +0100

    refact:changed header main to fit pointers integration

--- Headers/main.h
+++ Headers/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+typedef struct Student Student;
 struct Student {
-    char name[100];
-    char surname[100];
-    char city[100];
-    char address[100];
+    char *name;
+    char *surname;
+    char *address;
     int age;
 };
 #endif //MAIN_H
--- Sources/main.c
+++ Sources/main.c
@@ -7,9 +7,13 @@
 
 int main(int argc, char *argv[])
 {
-    struct Student student;
+    Student student;
+    student.name = "John";
+    student.surname = "Doe";
+    student.address = "123 rue de la Paix";
+    student.age = 18;
 
-
+    printf("The student named %s %s, %d years old, lives in %s.\n", student.name, student.surname, student.age, student.address);
     return 0;
 }
 
